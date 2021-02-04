0e1cd4d73375ab5cd7effbd0f839b21912aee474
swaldom <svichkar.dev@gmail.com>
2021-02-04 22:24:46 +0200

    homework 4

---
+++ hw4/string_practice.py
@@ -0,0 +1,22 @@
+string = 'Lorem, Ipsum, is, simply, dummy, text, of, the, printing, industry.'
+
+# 1. Изменить строку таким образом, чтоб вместо ', ' был пробел ' '
+#    Результат: 'Lorem Ipsum is simply dummy text of the printing industry.'
+
+
+# 2. Найти индекс самой последней буквы 's' в строке.
+#    Результат: 53
+
+
+# 3. Найти количество букв 'i' в строке (регистр не имеет значения).
+#    Результат: 6
+
+
+# 4. Найти и вывести срез строки.
+#    Результат: 'simply dummy text'
+#    (используйте методы find или index для получения индексов)
+
+
+# 5. Продублируйте первую половину строки 3 раза и склейте с второй половиной
+#    и выведите на экран.
+#    Результат: 'Lorem Ipsum is simply dummy tLorem Ipsum is simply dummy tLorem Ipsum is simply dummy text of the printing industry.'
