d59b4c4314ddb79f65d5f5a47d40ba734e8485de
swaldom <svichkar.dev@gmail.com>
2021-02-15 23:12:55 +0200

    homework 7

---
+++ hw7/lucky_ticket.py
@@ -0,0 +1,21 @@
+"""
+    Написать функцию, которая будет проверять счастливый билетик или нет.
+
+    Билет счастливый, если сумма одной половины цифр равняется сумме второй.
+"""
+
+
+def is_lucky(ticket_num):
+    pass
+
+
+assert is_lucky(1230) is True
+assert is_lucky(239017) is False
+assert is_lucky(134008) is True
+assert is_lucky(15) is False
+assert is_lucky(2020) is True
+assert is_lucky(199999) is False
+assert is_lucky(77) is True
+assert is_lucky(479974) is True
+
+print("All tests passed successfully!")
