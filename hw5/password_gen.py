6bcb59c3b56add342a95d90d5c656427f7a67ebe
swaldom <svichkar.dev@gmail.com>
2021-02-15 22:51:01 +0200

    homework 5 (solutions): password_gen.py

--- hw5/password_gen.py
+++ hw5/password_gen.py
@@ -21,11 +21,70 @@
         - программа генерирует пароль из нужной длины из введенных символов
         - * игнорируются пробелы
 """  # noqa: E501
+import random
+
+from string import ascii_lowercase, ascii_letters, digits, punctuation
 
 
 def main():
-    pass
+    choice = input(
+        "1. Сгенерировать простой пароль\n"
+        "2. Сгенерировать средний пароль\n"
+        "3. Сгенерировать сложный пароль\n"
+    )
+    if choice == "1":
+        # password = gen_simple_pw()
+        password = gen_password(ascii_lowercase)
+    elif choice == "2":
+        password = gen_password(ascii_letters + digits)
+        # password = gen_medium_pw()
+    elif choice == "3":
+        password = gen_strong_pw()
+
+    print(password)
 
 
-if __name__ == '__main__':
+# def gen_simple_pw():
+#     password = ""
+#     for i in range(8):
+#         password += random.choice(ascii_lowercase)
+#     return password
+#     # return "".join(random.sample(ascii_lowercase, 8))
+
+
+# def gen_medium_pw():
+#     password = ""
+#     for i in range(8):
+#         password += random.choice(ascii_letters + digits)
+#     return password
+
+
+def gen_password(chars, length=8):
+    password = ""
+    for i in range(length):
+        password += random.choice(chars)
+    return password
+
+
+def gen_strong_pw():
+    length = random.randint(8, 16)
+    password = gen_password(digits + ascii_letters + punctuation, length)
+
+    counter_d = counter_u = counter_l = counter_s = 0
+    for char in password:
+        if char.isdigit():
+            counter_d += 1
+        elif char.isupper():
+            counter_u += 1
+        elif char.islower():
+            counter_l += 1
+        elif not char.isspace():
+            counter_s += 1
+
+    if counter_d and counter_u and counter_l and counter_s:
+        return password
+    return gen_strong_pw()
+
+
+if __name__ == "__main__":
     main()
