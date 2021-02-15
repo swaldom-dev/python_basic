c014f2672cf409ccbf926a9a9a54afb52fa58c2a
swaldom <svichkar.dev@gmail.com>
2021-02-15 21:42:50 +0200

    lesson 7 (part 2): methods, practice, tuples

--- lesson7/_3_pratice.py
+++ lesson7/_3_pratice.py
@@ -3,3 +3,17 @@
 """
 
 a = ["1", "3.14", "w", "one", "w3c", "123", "a"]
+
+# 1 способ
+b = []
+for elem in a:
+    if elem.isdigit():
+        b.append(int(elem))
+
+print(b)
+
+# 2 способ
+
+b = [int(elem) for elem in a if elem.isdigit()]
+
+print(b)
