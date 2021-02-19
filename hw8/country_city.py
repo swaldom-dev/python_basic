5669855ee3013b4f285bd8e0cba97a484b267b06
swaldom <svichkar.dev@gmail.com>
2021-02-19 13:46:58 +0200

    homework 8

---
+++ hw8/country_city.py
@@ -0,0 +1,25 @@
+"""
+    1. Реализовать функцию get_country(city), которая принимает название города
+    и возвращает страну, которой он принадлежит исходя из словаря data.
+
+    2. Релизовать функцию groupping_data(data), которая принимает словарь data
+    и возвращает отформатированные данные в виде списка словарей с ключами
+    'country', 'capital' и 'cities'.
+    Учитывать, что во входящем словаре data
+    ключ - country, первый элемент значения - capital, остальные - cities.
+"""
+
+data = {
+    "Ukraine": ["Kiev", "Kharkiv", "Odesa", "Dnipro"],
+    "France": ["Paris", "Marseille", "Lyon", "Toulouse"],
+    "Austria": ["Vienna", "Graz", "Linz", "Salzburg"],
+    "Germany": ["Berlin", "Hamburg", "Munich", "Frankfurt"],
+}
+
+
+def get_country(city):
+    pass
+
+
+def groupping_data(data):
+    pass
