e8595027dc36cd0c7c70f6cff21698dae5003f20
swaldom <svichkar.dev@gmail.com>
2021-02-23 12:46:30 +0200

    homework 6 (solutions)

--- hw6/file_practice.py
+++ hw6/file_practice.py
@@ -5,11 +5,21 @@
     Файл должен заканчиваться пустой строкой
 """
 
+with open("file_practice.txt", "w") as f:
+    print("Starting practice with files", file=f)
+    # or
+    # f.write('Starting practice with files\n')
+
 """
     2.
     Прочесть первые 5 символов файла и вывести содержимое в верхнем регистре
     Затем прочесть весь файл от начала до конца, вывести содержимое на экран
 """
+
+with open("file_practice.txt") as f:
+    print(f.read(5).upper())
+    f.seek(0)
+    print(f.read())
 
 """
     3.
@@ -19,6 +29,16 @@
     Полученный текст дописать в файл file_practice.txt
 """
 
+with open("files/text.txt") as f:
+    text = f.read()
+    if text.count("i") > text.count("e"):
+        text = text.replace("i", "e")
+    else:
+        text = text.replace("e", "i")
+
+with open("file_practice.txt", "a") as f:
+    f.write(text)
+
 """
     4.
     Если в файле file_practice.txt четное количество элементов
@@ -26,8 +46,26 @@
     Прочесть весь файл и вывести содержимое
 """
 
+with open("file_practice.txt", "a+") as f:
+    f.seek(0)
+    text = f.read()
+    if len(text) % 2 == 0:
+        f.write("the end")
+    else:
+        f.write("bye")
+
+    f.seek(0)
+    print(f.read())
+
 """
     5.
     В средину файла file_practice.txt вставить строку " *some inserted text* "
     (средина - имеется в виду средина текста)
 """
+
+with open("file_practice.txt", "r+") as f:
+    text = f.read()
+    f.seek(0)
+    center = len(text) // 2
+    text = text[:center] + "*some inserted text*" + text[center:]
+    f.write(text)
--- hw6/phone_book.py
+++ hw6/phone_book.py
@@ -10,11 +10,43 @@
     3. +380501234563 - Имя
     4. +380501234564 - Имя
 """
+from pathlib import Path
+
+FILES_DIR = Path(__file__).resolve().parent / "files"
 
 
 def main():
-    pass
+    with open(FILES_DIR / "phone_book.txt") as f_in:
+        with open(FILES_DIR / "edited_phone_book.txt", "a") as f_out:
+            num = 1
+            for line in f_in.readlines():
+                name = "".join(char for char in line if char.isalpha()).lower()
+
+                if name.startswith("m") or name.endswith("a"):
+                    phone = "".join(char for char in line if char.isdigit())
+                    phone = "+380" + phone[-9:]
+                    print(f"{num}. {phone} - {name.title()}", file=f_out)
+                    num += 1
 
 
 if __name__ == "__main__":
     main()
+
+# import re
+
+# with open("hw6/files/phone_book.txt") as f:
+#     idx = 1
+#     for line in f.readlines():
+#         # получить имя
+#         # 1 вариант в две строки
+#         # name_phone = re.sub(r"\W", "", line)
+#         # name = re.sub(r"\d", "", name_phone).lower()
+
+#         # 2 вариант в одну строку
+#         name = re.sub(r"(\W|\d)", "", line).lower()
+
+#         if name[0] == "m" or name[-1] == "a":
+#             phone = "+380" + re.sub(r"\D", "", line)[-9:]
+#             with open("hw6/files/edited_phone_book.txt", "a") as f:
+#                 print(f"{idx}. {phone} - {name.title()}", file=f)
+#                 idx += 1
